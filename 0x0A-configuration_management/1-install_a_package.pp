34f6ab2a11fcd3760ae12f107af2df0abf9ac923
Abderrahmane369 <cszamasu@gmail.com>
2023-12-23 01:07:38 +0100

    Updated_Something

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # task 2
 package { 'flask':
-  ensure   => 2.1.0,
-  provider => pip3,
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
 package { 'werkzeug':
-ensure   => 2.1.1,
-provider => pip3,
+ensure   => '2.1.1',
+provider => 'pip3',
 }

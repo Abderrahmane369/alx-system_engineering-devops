7be319b1fdd9ddc9845e36085deebc1fdcf46bf8
Abderrahmane369 <cszamasu@gmail.com>
2024-01-31 01:26:45 +0100

    Updated_Something

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,18 +4,20 @@ import requests as rq
 import json
 from sys import argv
 
-users = rq.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
-todos = rq.get(f'https://jsonplaceholder.typicode.com/todos/')
-todos = json.loads(todos.content.decode())
+if __name__ == '__main__':
+    users = rq.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    todos = rq.get(f'https://jsonplaceholder.typicode.com/todos/')
+    todos = json.loads(todos.content.decode())
 
-employee = json.loads(users.content.decode())
+    employee = json.loads(users.content.decode())
 
-emptodos = list(filter(lambda e: e.get('userId') == employee.get('id'), todos))
+    emptodos = list(filter(lambda e: e.get('userId')
+                    == employee.get('id'), todos))
 
-tasksDone = list(filter(lambda e: e.get('completed'), emptodos))
+    tasksDone = list(filter(lambda e: e.get('completed'), emptodos))
 
-print(f'Employee {employee.get("name")} ' +
-      f'is done with tasks({len(tasksDone)}/{len(emptodos)}):')
+    print(f'Employee {employee.get("name")} ' +
+          f'is done with tasks({len(tasksDone)}/{len(emptodos)}):')
 
-for i in tasksDone:
-    print(f'\t {i.get("title")}')
+    for i in tasksDone:
+        print(f'\t {i.get("title")}')

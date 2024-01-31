7bfe9b67925aa1a1f61b950703d960ce4c7431b2
Abderrahmane369 <cszamasu@gmail.com>
2024-01-31 02:03:20 +0100

    Updated_Something

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""logar"""
+import json
+import requests as rq
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    users = rq.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    todos = rq.get(f'https://jsonplaceholder.typicode.com/todos/')
+    todos = json.loads(todos.content.decode())
+
+    employee = json.loads(users.content.decode())
+
+    emptodos = list(filter(lambda e: e.get('userId')
+                    == employee.get('id'), todos))
+
+    with open(f'{argv[1]}.py', 'w', encoding='utf-8') as f:
+        writer = csv.writer(f)
+
+        for t in emptodos:
+            writer.writerow((t['userId'], employee['username'],
+                             t['completed'], t['title']))

5d161ea5cbb76c3503806e22220ae3899a1faa7d
Abderrahmane369 <cszamasu@gmail.com>
2024-02-13 22:48:27 +0100

    Updated_Something

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""requestsREDDIT"""
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """READIT"""
+    headers = {'User-agent': 'redddit'}
+
+    response = requests.get(
+        f'https://www.reddit.com/r/{sys.argv[1]}/about.json',
+        headers=headers
+    )
+
+    data = response.json()
+
+    if not data.get('data').get('subscribers'):
+        return 0
+
+    return data['data']['subscribers']
